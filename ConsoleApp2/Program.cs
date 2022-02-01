4624837dad4d58dd841b946a4e47e999a6c0366b
John Hinz <jhinz402@hotmail.com>
2022-01-31 21:20:34 -0500

    initial commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SomeName
+{
+
+	public class Program
+	{
+		public static void Main()
+		{
+
+            Console.WriteLine("Please enter a sequence:");
+			string userInput = Console.ReadLine();
+			string[] arrayInput = userInput.Split('+');
+			int sum = 0;
+
+
+			foreach (string y in arrayInput)
+            {
+				sum = int.Parse(y) + sum;
+			}
+
+			//for (int i = 0; i < arrayInput.Length; i++)
+   //         {
+			//	sum = int.Parse(arrayInput[i]) + sum;
+			//}
+
+            Console.WriteLine($"The sumation of your input is: {sum}");
+
+		}
+	}
+
+}
